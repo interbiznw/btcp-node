198af954589013b69d276a6c184b09b4fa63a439
J62 <j.owens@interbiznw.com>
2021-04-02 01:25:00 -0700

    fix port

--- terraform/gcloud/firewall.tf
+++ terraform/gcloud/firewall.tf
@@ -3,7 +3,7 @@ resource "google_compute_firewall" "btcp-terraform" {
   network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["17933, 22"]
+    ports    = ["17933", "22"]
   }
   source_tags = ["terraformnodes"]
   source_ranges = ["0.0.0.0/0"]
