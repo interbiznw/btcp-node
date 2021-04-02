92ba20e274bfd3f2fa26ecb5fc9a4b892aa3f9b8
J62 <j.owens@interbiznw.com>
2021-04-02 03:51:27 -0700

    enable service

--- terraform/gcloud/startup.sh
+++ terraform/gcloud/startup.sh
@@ -13,5 +13,7 @@ wget https://raw.githubusercontent.com/interbiznw/btcp-node/b1d0313069823fb70211
 wget https://raw.githubusercontent.com/interbiznw/btcp-node/master/terraform/gcloud/fetch-params.sh -O /root/fetch-params.sh
 chmod +x /root/fetch-params.sh
 /bin/bash /root/fetch-params.sh
-cd /root && ./btcpd
+wget https://raw.githubusercontent.com/interbiznw/btcp-node/master/terraform/gcloud/btcpd.service -O /usr/lib/systemd/system/btcpd.service
+systemctl enable btcpd.service
+service btcpd start
 echo hi > /root/test.txt
